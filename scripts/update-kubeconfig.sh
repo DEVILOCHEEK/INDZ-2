b9ad88853b31510923bdbcae5125efa9ba396627
DEVILOCHEEK <123664311+DEVILOCHEEK@users.noreply.github.com>
2025-05-22 10:32:40 +0300

    Delete scripts directory

--- scripts/update-kubeconfig.sh
+++
@@ -1,8 +0,0 @@
-CLUSTER_NAME="my-eks-cluster"
-REGION="us-west-2"
-
-echo "Updating kubeconfig for cluster $CLUSTER_NAME..."
-
-aws eks update-kubeconfig --name $CLUSTER_NAME --region $REGION
-
-echo "kubeconfig updated."
